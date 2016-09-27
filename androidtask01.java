6ca38a894f3aee265321a5a06ebc194ff943987d
oyla <oyla@list.ru>
2016-09-27 11:16:05 +0300

    Create androidtask01.java

---
+++ androidtask01.java
@@ -0,0 +1,23 @@
+package com.example.user.androidtask01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        button1.setWidth(button1.getWidth() + 20);
+    }
+}
